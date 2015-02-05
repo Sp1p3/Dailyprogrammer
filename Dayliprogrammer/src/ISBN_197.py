8d10afd074de9b759b1c9595d92fbbeaaf6b0500
canillas <canillas@nuabee-lca>
2015-02-05 11:47:24 +0100

    Mise de la solution dans le bon fichier.py

--- Dayliprogrammer/src/ISBN_197.py
+++ Dayliprogrammer/src/ISBN_197.py
@@ -7,6 +7,20 @@
 __author__ = "canillas"
 __date__ = "$5 fevr. 2015 10:52:12$"
 
-if __name__ == "__main__":
-   
+def CheckIsbn(tab):
+    somme=0
+    j=len(tab)
+    for i in tab:
+        somme=somme+(i*j)
+        j=j-1
     
+    if ((somme % 11) == 0 ):
+        print("isbn")
+    else:
+        print("pas isbn")
+   
+if __name__ == "__main__":
+    tab= [0,7,4,7,5,3,2,6,9,9]
+    CheckIsbn(tab)
+    tab=[0,5,4,5,0,1,0,2,2,5]
+    CheckIsbn(tab)
