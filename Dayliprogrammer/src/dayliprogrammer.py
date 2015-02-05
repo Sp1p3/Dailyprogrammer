ee0a7521c68e005692f97673e40ef60b25790cee
canillas <canillas@nuabee-lca>
2015-02-05 10:51:25 +0100

    Mise en place du GIT

---
+++ Dayliprogrammer/src/dayliprogrammer.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$5 fevr. 2015 10:50:25$"
+
+if __name__ == "__main__":
+    print("Hello World")
