37d975a5274e78f395eca9274f14694494a69195
canillas <canillas@nuabee-lca>
2015-02-05 11:54:32 +0100

    Utilisation du module ISBN_197

--- Dayliprogrammer/src/dayliprogrammer.py
+++ Dayliprogrammer/src/dayliprogrammer.py
@@ -4,6 +4,7 @@
 
 __author__ = "canillas"
 __date__ = "$5 fevr. 2015 10:50:25$"
+import ISBN_197
 
 
 
@@ -14,6 +15,8 @@ if __name__ == "__main__":
     
         
     print("Hello 197 DONE")
+    tab= [0,7,4,7,5,3,2,6,9,9]
+    ISBN_197.CheckIsbn(tab)
 
     
     
