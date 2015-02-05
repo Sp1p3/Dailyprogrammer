9c30b515173c972906b3225ece1af8be63c8c6dd
canillas <canillas@nuabee-lca>
2015-02-05 10:52:45 +0100

    Creation de la page pour le defi 197

---
+++ Dayliprogrammer/src/197_ISBN _Validator.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$5 fevr. 2015 10:52:12$"
+
+if __name__ == "__main__":
+    print "Hello World";
