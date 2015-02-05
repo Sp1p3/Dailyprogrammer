86e54bd18f51338c7013404250b329e74525662f
canillas <canillas@nuabee-lca>
2015-02-05 11:20:00 +0100

    Brouillon de solution

--- Dayliprogrammer/src/dayliprogrammer.py
+++ Dayliprogrammer/src/dayliprogrammer.py
@@ -8,4 +8,16 @@ __date__ = "$5 fevr. 2015 10:50:25$"
 
 if __name__ == "__main__":
     print("Hello 197")
+    somme=0
+    j=0
+    tab = [0,7,4,7,5,3,2,6,9,9]
     
+    for i in tab:
+        j=j+1
+        somme=somme+(i*j)
+    
+    print(somme)
+    if ((somme % 11) == 0 ):
+        print("isbn")
+    else:
+        print("pas isbn")
