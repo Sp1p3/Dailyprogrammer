b3eb41b874d58a1d64f5dafb1560a374f0cbdf7c
canillas <canillas@nuabee-lca>
2015-02-05 11:43:52 +0100

    Mise en fonction de la solution

--- Dayliprogrammer/src/dayliprogrammer.py
+++ Dayliprogrammer/src/dayliprogrammer.py
@@ -6,18 +6,28 @@ __author__ = "canillas"
 __date__ = "$5 fevr. 2015 10:50:25$"
 
 
-if __name__ == "__main__":
-    print("Hello 197")
+
+    
+def CheckIsbn(tab):
     somme=0
-    j=0
-    tab = [0,7,4,7,5,3,2,6,9,9]
-    
+    j=len(tab)
     for i in tab:
-        j=j+1
         somme=somme+(i*j)
+        j=j-1
     
-    print(somme)
     if ((somme % 11) == 0 ):
         print("isbn")
     else:
         print("pas isbn")
+   
+
+if __name__ == "__main__":
+    
+        
+    print("Hello 197")
+    tab= [0,7,4,7,5,3,2,6,9,9]
+    CheckIsbn(tab)
+    tab=[0,5,4,5,0,1,0,2,2,5]
+    CheckIsbn(tab)
+    
+    
