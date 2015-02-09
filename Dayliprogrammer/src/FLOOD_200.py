be7b6459e774894d835ff08b60b50c89012070d5
canillas <canillas@nuabee-lca>
2015-02-09 15:39:47 +0100

    Spécification des fonctions de Flood

--- Dayliprogrammer/src/FLOOD_200.py
+++ Dayliprogrammer/src/FLOOD_200.py
@@ -11,4 +11,26 @@ __date__ = "$5 fevr. 2015 16:41:56$"
 
 def hello():
     print("je suis en cours de travail")
+
+def tableau(tab):
+    return 0 
     
+    
+def decoupage(tab):
+    return 0
+# decoupage du tableau en tableau contenant les parties 
+# des que je trouve un caractere different  je fais un tableau et je met un duet de coordonnée dans un tableau 
+# faire un duet avec un caractere de séparation fort 
+# 
+#input : tableau 2D caracteres contenant les zones 
+#output : tableau 2D caracteres contenant par zones les coordonnés de chaques zones et les caracteres
+#
+
+
+
+def remplacement(tab,char,partie=0):
+    return 0 
+# remplacement de la partie du tableau concernée 
+
+
+ 
