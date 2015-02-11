a274a7fa6c29495e103e5524825d119ab484cfee
canillas <canillas@nuabee-lca>
2015-02-11 10:55:17 +0100

    Refactorisation de la main, Ajouts du fichier Shell_shock pour les travaux relatifs

---
+++ Dayliprogrammer/src/SHELL_SHOCK.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$11 fevr. 2015 10:50:30$"
+
+import sys
+import urllib2
+
+def toto():
+    print("hello")
+    
+
