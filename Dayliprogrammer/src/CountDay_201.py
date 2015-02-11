a83638b9a3cac89f5d3a6ebaff1483d85465d047
canillas <canillas@nuabee-lca>
2015-02-11 11:58:19 +0100

    Creation du fichier relatif au Defi 201

---
+++ Dayliprogrammer/src/CountDay_201.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+# 
+# http://www.reddit.com/r/dailyprogrammer/comments/2vc5xq/20150209_challenge_201_easy_counting_the_days/
+# 
+
+__author__ = "canillas"
+__date__ = "$11 fevr. 2015 11:56:27$"
+
+if __name__ == "__main__":
+    print "Hello World";
+
+def hello():
+    print("hello)
