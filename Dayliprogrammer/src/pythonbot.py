a5682efbd2608e0efc2aef70a72342a7038be5d7
canillas <canillas@nuabee-lca>
2015-02-13 13:24:09 +0100

    Creation du bot IRC (pour le club secu)

---
+++ Dayliprogrammer/src/pythonbot.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$13 fevr. 2015 09:55:06$"
+
+import sys
+import socket
+import string
+
+def run():
+    HOST="irc.clubsecu.fr"
+    PORT=6667
+    NICK="Sp1p3-Bot3000"
+    IDENT="BOT"
+    REALNAME="Sp1p3-Bot"
+    readbuffer=""
+    out="0"
+
+
+    s=socket.socket( )
+    s.connect((HOST, PORT))
+    s.send("NICK %s\r\n" % NICK)
+    s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+
+    while 1:
+        readbuffer=readbuffer+s.recv(1024)
+        temp=string.split(readbuffer, "\n")
+        readbuffer=temp.pop( )
+
+        for line in temp:
+            line=string.rstrip(line)
+            line=string.split(line)
+
+            if(line[0]=="PING"):
+                s.send("PONG %s\r\n" % line[1])
+            
+            print(line)
+            
+            
+            for word in line: 
+                if(word ==":TOTO"):
+                    s.send("PRIVMSG #resir TATA \r\n")
+                    
+                    
+    
+           
